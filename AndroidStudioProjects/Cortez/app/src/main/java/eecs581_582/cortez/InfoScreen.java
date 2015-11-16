9178b324e454623d6cd5b85c87445349e12d34d6
slamb <slamb@ku.edu>
2015-11-16 13:07:24 -0600

    Boilerplate stuff for InfoScreen.java

--- AndroidStudioProjects/Cortez/app/src/main/java/eecs581_582/cortez/InfoScreen.java
+++ AndroidStudioProjects/Cortez/app/src/main/java/eecs581_582/cortez/InfoScreen.java
@@ -1,7 +1,54 @@
 package eecs581_582.cortez;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by slamb on 11/16/15.
  */
-public class InfoScreen {
+public class InfoScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment()).commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle app bar item clicks here. The app bar
+        // automatically handles clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() { }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_display_message,
+                    container, false);
+            return rootView;
+        }
+    }
+
 }
+
