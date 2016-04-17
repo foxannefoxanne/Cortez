6f0c6fb28e9b6e1ee713d3994813c624f958e6dd
jdchampion <champion@ku.edu>
2016-04-16 23:46:56 -0500

    Using tabbed view with labels

--- AndroidStudioProjects/Cortez/app/src/main/java/eecs581_582/cortez/frontend/MediaSelectActivity.java
+++ AndroidStudioProjects/Cortez/app/src/main/java/eecs581_582/cortez/frontend/MediaSelectActivity.java
@@ -1,6 +1,5 @@
 package eecs581_582.cortez.frontend;
 
-
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
@@ -20,7 +19,7 @@ import android.widget.TextView;
 
 import eecs581_582.cortez.R;
 
-public class MediaSelectActivity extends Activity {
+public class MediaSelectActivity extends Activity implements ActionBar.TabListener {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -50,6 +49,32 @@ public class MediaSelectActivity extends Activity {
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
 
+        // Set up the action bar.
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+
     }
 
 
@@ -73,6 +98,21 @@ public class MediaSelectActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
 
     /**
@@ -137,11 +177,11 @@ public class MediaSelectActivity extends Activity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "IMAGE";
                 case 1:
-                    return "SECTION 2";
+                    return "AUDIO";
                 case 2:
-                    return "SECTION 3";
+                    return "VIDEO";
             }
             return null;
         }
